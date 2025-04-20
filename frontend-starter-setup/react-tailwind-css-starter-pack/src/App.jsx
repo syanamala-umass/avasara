b7fd0d2ab31d85148821747608e45dd1ab60d798
syanamala-umass <syanamala@umass.edu>
2025-04-20 15:40:59 -0400

    added tasks,idea page

--- frontend-starter-setup/react-tailwind-css-starter-pack/src/App.jsx
+++ frontend-starter-setup/react-tailwind-css-starter-pack/src/App.jsx
@@ -1,10 +1,19 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './LandingPage';
 import UserDashboard from './UserDashboard';
+import IdeaDetailPage from './IdeaDetailPage';
 
 function App() {
-  // return <LandingPage />;
-  return <UserDashboard />
+  return (
+
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<UserDashboard />} />
+        <Route path="/ideas/:id" element={<IdeaDetailPage />} />
+      </Routes>
+
+  );
 }
 
 export default App;
